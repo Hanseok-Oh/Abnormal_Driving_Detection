2006bd70a12c6a1a7d167f22f82fde7b81b11b6d
YONGSUN YOON <respect5716@gmail.com>
2019-06-04 23:53:55 +0900

    train.py 수정
    weight 저장 기능 추가

--- train.py
+++ train.py
@@ -20,10 +20,13 @@ from modules.models import autoencoder
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--array_path", help='array 경로', type=str)
+parser.add_argument("--array_path", help='array path', type=str)
+parser.add_argument("--weight_path", help='weight path', type=str)
 
 args = parser.parse_args()
 array_path = args.array_path
+weight_path = args.weight_path
+
 array_data = np.load(array_path)
 array_data = np.transpose(array_data, (0, 3, 1, 2))
 
@@ -40,13 +43,13 @@ dataloader = DataLoader(
     shuffle=True)
 
 # model
-auto_encoder = autoencoder.AutoEncoder()
-auto_encoder.to(device)
-#print(summary(auto_encoder, (3, 240,320)))
+model = autoencoder.AutoEncoder()
+model.to(device)
+#print(summary(model, (3, 240,320)))
 
 # train
 criterion = nn.MSELoss().to(device) # loss function
-optimizer = optim.Adam(auto_encoder.parameters(), lr=1e-3) # adam optimizer
+optimizer = optim.Adam(model.parameters(), lr=1e-3) # adam optimizer
 epoch = 2
 
 print("학습 시작")
@@ -57,7 +60,7 @@ for ep in range(epoch):
         data = data.to(device)
 
         optimizer.zero_grad()
-        outputs = auto_encoder(data)
+        outputs = model(data)
 
         loss = criterion(outputs, data)
         loss.backward()
@@ -69,3 +72,6 @@ for ep in range(epoch):
     ep_loss = loss.item() / batch
     print("epoch:{} / loss:{} / time:{}".format(ep, ep_loss, ep_time))
 print("학습 완료")
+
+# 모델 저장
+torch.save(model.state_dict(), weight_path)
