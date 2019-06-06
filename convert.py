444d74d7ce8415a8a5523aa2a0610d84f26fe22f
YONGSUN YOON <respect5716@gmail.com>
2019-06-06 13:05:19 +0900

    이미지 파일로 변환
    npy파일 대신 이미지 파일은 나눠서 저장

--- convert.py
+++ convert.py
@@ -4,8 +4,6 @@ import cv2
 import torch
 import numpy as np
 import pandas as pd
-import skvideo.io
-
 
 #argparse
 import argparse
@@ -13,16 +11,14 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("--load_path", help='load path', type=str)
 parser.add_argument("--save_path", help='save path', type=str)
-parser.add_argument('--dataset_name', help='dataset name', type=str)
 
 args = parser.parse_args()
 load_path = args.load_path
 save_path = args.save_path
-dataset_name = args.dataset_name
 
 # Convert class
 class Convert():
-    def __init__(self, load_path=load_path, save_path=save_path, dataset_name=dataset_name):
+    def __init__(self, load_path=load_path, save_path=save_path):
         self.load_path = load_path
         self.save_path = save_path
         self.dataset_name = dataset_name
@@ -44,40 +40,23 @@ class Convert():
         self.current_video_frame_num = int(cap.get(cv2.cv2.CAP_PROP_FRAME_COUNT)) # frame 수
 
         # capture
-        current_video_frame = []
-        while True:
+        for i in range(self.current_video_frame_num):
             ret, frame = cap.read()
-            if not ret:
-                break
 
-            current_video_frame.append(frame) # frame을 video_frame에 저장
+            img_name = "{}_{}.png".format(num, i)
+            os.chdir(save_path) # save path로 이동
+            cv2.imwrite(img_name, frame) # frame to img
 
             if cv2.waitKey(30) == 27:
                 break
+
         cap.release()
         cv2.destroyAllWindows()
-        self.current_video_frame = np.array(current_video_frame, dtype=np.float32) # 현재 처리 video frame
-
-    # 영상 불러오기 2
-    def cap_video_2(self, num):
-        current_video = self.videos[num] # 현재 처리 video
-        current_video_path = self.load_path + current_video
-        self.current_video_frame = skvideo.io.vread(current_video_path)
 
     # convert dataset
     def convert_dataset(self):
         for i in range(len(self.videos)):
             self.cap_video(i)
-            #self.cap_video_2(i)
-            if i == 0:
-                dataset = self.current_video_frame
-            else:
-                dataset = np.concatenate((dataset, self.current_video_frame))
-
-            if i % 10 == 0:
-                print("{}% 변환 완료".format(str(int(((i+1) / len(self.videos)) * 100))))
-
-        self.dataset = dataset
 
     # dataset info
     def dataset_info(self):
@@ -85,15 +64,10 @@ class Convert():
         times = [i.split('_')[1] for i in self.videos]
         names = [i.split('_')[2] for i in self.videos]
         self.datainfo = pd.DataFrame({'Day':days, 'Time':times, 'CCTV_Name':names})
-
-    # save dataset
-    def save_dataset(self):
-        np.save(file=self.save_path + self.dataset_name + '.npy', arr=self.dataset)
-        self.datainfo.to_csv(self.save_path + self.dataset_name + '.csv' ,encoding='utf-8')
+        self.datainfo.to_csv(self.save_path + 'data_info.csv', encoding='utf-8')
 
 if __name__ == '__main__':
     c = Convert()
     c.convert_dataset()
     c.dataset_info()
-    c.save_dataset()
     print('변환 완료')
