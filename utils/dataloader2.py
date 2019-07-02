35621a2da32d5e6c9f9e7e4fc47f96542aaf6aa5
YONGSUN YOON <respect5716@gmail.com>
2019-07-02 11:55:05 +0900

    update

--- utils/dataloader2.py
+++ utils/dataloader2.py
@@ -29,29 +29,28 @@ class DataLoader:
             X = X.astype(float) / 255
             yield (X, X)
 
-)
 
-    def rnn_loader(self, graph, encoder, offset_x, offset_y):
-        with graph.as_default():
-            while True:
-                selected_video = self._choose_random_video()
 
-                for i in range(len(selected_video)):
-                    video = selected_video[i]
-                    frame_x, frame_y = self._choose_rnn_frame(video, offset_x, offset_y)
-                    latent_x = np.array([encoder.predict(i) for i in frame_x])
-                    latent_y = encoder.predict(frame_y)
+    def rnn_loader(self, offset_x, offset_y):
 
-                    if i == 0:
-                        X = latent_x
-                        Y = latent_y
-                    else:
-                        X = np.concatenate((X, latent_x))
-                        Y = np.concatenate((Y, latent_y))
+        while True:
+            selected_video = self._choose_random_video()
 
-                X = X.astype('float32') / 255
-                Y = Y.astype('float32') / 255
-                yield (X, Y)
+            for i in range(len(selected_video)):
+                video = selected_video[i]
+                frame_x, frame_y = self._choose_rnn_frame(video, offset_x, offset_y)
+
+
+                if i == 0:
+                    X = frame_x
+                    Y = frame_y
+                else:
+                    X = np.concatenate((X, frame_x))
+                    Y = np.concatenate((Y, frame_y))
+
+            X = X.astype('float32') / 255
+            Y = Y.astype('float32') / 255
+            yield ([X[:,i,:,:,:] for i in range(offset_x)], Y)
 
 
     def _choose_random_video(self):
@@ -77,7 +76,7 @@ class DataLoader:
         selected_index_y = np.random.randint(offset_y, len(frame_list), self.batch_per_video)
         selected_index_x = [[y - offset_y + x for x in range(offset_x)] for y in selected_index_y]
         selected_frame_y = np.array([idx_to_array(i, video_path) for i in selected_index_y])
-        selected_frame_x = np.array([[idx_to_array(i, video_path) for i in l] for l in selected_index_x])
+        selected_frame_x = [[idx_to_array(i, video_path) for i in l] for l in selected_index_x]
         return selected_frame_x, selected_frame_y
 
 
