8a78f32a0c0f71602f2bd69aa41e1bdfbcec011d
YONGSUN YOON <respect5716@gmail.com>
2019-07-02 10:05:36 +0900

    update

--- models/recurrent.py
+++ models/recurrent.py
@@ -1,9 +1,9 @@
 import keras.layers as L
 
-def RNN_v1(offset_x, input_shape):
+def RNN_v1(offset_x, rnn_input):
     rnn = L.Sequential()
-    rnn.add(L.LSTM(512, input_shape=(offset_x, input_shape)))
-    rnn.add(L.Dense(6144))
+    rnn.add(L.LSTM(512, input_shape=(offset_x, rnn_input)))
+    rnn.add(L.Dense(rnn_input))
     return rnn
 
 def RNN_v2(offset_x, input_shape):
