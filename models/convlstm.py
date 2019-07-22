fd661c80650b2d397fc27a39be94ac1d6c325559
YONGSUN YOON <respect5716@gmail.com>
2019-07-22 13:01:56 +0900

    update

--- models/convlstm.py
+++ models/convlstm.py
@@ -4,11 +4,12 @@ import keras.layers as L
 def ConvLSTM(offset_x):
     inputs = L.Input((offset_x, 256, 256, 1), name='inputs')
     x = L.ConvLSTM2D(32, (3,3), strides=2, padding='same', activation='relu', kernel_initializer='he_uniform', return_sequences=True)(inputs)
+    x = L.ConvLSTM2D(32, (3,3), strides=2, padding='same', activation='relu', kernel_initializer='he_uniform', return_sequences=True)(x)
     x = L.ConvLSTM2D(64, (3,3), strides=2, padding='same', activation='relu', kernel_initializer='he_uniform', return_sequences=True)(x)
     x = L.ConvLSTM2D(64, (3,3), strides=2, padding='same', activation='relu', kernel_initializer='he_uniform', return_sequences=True)(x)
-    encoded = L.ConvLSTM2D(128, (3,3), strides=2, padding='same', activation='relu', kernel_initializer='he_uniform', return_sequences=False, name='encoded')(x)
+    x = L.ConvLSTM2D(128, (3,3), strides=2, padding='same', activation='relu', kernel_initializer='he_uniform', return_sequences=False, name='encoded')(x)
 
-    x = L.Conv2DTranspose(128, (2, 2), strides=2, padding='same', activation='relu', kernel_initializer='he_uniform')(encoded)
+    x = L.Conv2DTranspose(128, (2, 2), strides=2, padding='same', activation='relu', kernel_initializer='he_uniform')(x)
     x = L.Conv2D(128, (3,3), strides=1, padding='same', activation='relu', kernel_initializer='he_uniform')(x)
     x = L.Conv2DTranspose(64, (2, 2), strides=2, padding='same', activation='relu', kernel_initializer='he_uniform')(x)
     x = L.Conv2D(64, (3,3), strides=1, padding='same', activation='relu', kernel_initializer='he_uniform')(x)
@@ -16,13 +17,9 @@ def ConvLSTM(offset_x):
     x = L.Conv2D(64, (3,3), strides=1, padding='same', activation='relu', kernel_initializer='he_uniform')(x)
     x = L.Conv2DTranspose(32, (2, 2), strides=2, padding='same', activation='relu', kernel_initializer='he_uniform')(x)
     x = L.Conv2D(32, (3,3), strides=1, padding='same', activation='relu', kernel_initializer='he_uniform')(x)
+    x = L.Conv2DTranspose(32, (2, 2), strides=2, padding='same', activation='relu', kernel_initializer='he_uniform')(x)
+    x = L.Conv2D(32, (3,3), strides=1, padding='same', activation='relu', kernel_initializer='he_uniform')(x)
     x = L.Conv2D(1, (3,3), strides=1, padding='same', activation='sigmoid', kernel_initializer='he_uniform')(x)
 
     model = Model(inputs, x)
     return model
-
-def Encoder(convlstm):
-    encoder = Model(
-        input = convlstm.get_layer('inputs').input,
-        output = convlstm.get_layer('encoded').output)
-    return encoder
