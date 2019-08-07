fafd4c80a8e7a473d1fb9b69743ca2743630a9f8
YONGSUN YOON <respect5716@gmail.com>
2019-08-07 23:52:03 +0900

    dataloader update
    step별로 독립된 형태로 출력

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 import argparse
-
 import utils
-from utils import dataloader
-from models import autoencoder, recurrent
+from utils.dataloader import DataLoader
 
 # argparse
 parser = argparse.ArgumentParser()
-parser.add_argument('--data_path', help='데이터 디렉토리', type=str)
-parser.add_argument('--train', help='train or test', type=str, defualt='train')
-parser.add_argument('--model', help='model', typ=str, default='autoencoder')
+parser.add_argument('--directory', help='데이터 디렉토리', type=str)
 parser.add_argument('--batch_size', help='batch size', type=int, default=32)
-parser.add_argument('--batch_per_video', help='batch per video', type=str, default=4)
-parser.add_argument('--model_path', help='model save/load path', type=str)
+parser.add_argument('--batch_per_video', help='batch per video', type=int, default=4)
 args = parser.parse_args()
 
 
@@ -22,11 +17,9 @@ def test():
     return
 
 def main(args):
-    train_loader = dataloader.DataLoader(args.data_path, batch_size=args.batch_size, batch_per_video=args.batch_per_video)
-
-    model_ae = autoencoder.Autoencoder()
-    rnn_dim = ae.get_layer('encoder').output_shape[1]
-    model_rnn = recurrent.RNN(model_ae, 3, rnn_dim)
+    loader = DataLoader(args.directory).random_batches()
+    x, y = next(loader)
+    print(x.shape)
 
 
 if __name__ == '__main__':
--- utils/dataloader.py
+++ utils/dataloader.py
@@ -1,80 +1,50 @@
 import os
 import cv2
-import random
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from PIL import Image
 
 class DataLoader:
-    def __init__(self, directory, batch_size=32, batch_per_video=4):
+    def __init__(self, directory, offset_x=[1,5,9], offset_y=50, batch_size=32, batch_per_video=4):
         self.directory = directory
+        self.videos = [os.path.join(self.directory, i) for i in os.listdir(directory)]
         self.batch_size = batch_size
         self.batch_per_video = batch_per_video
-        self.vidoe_list = os.listdir(directory)
+        self.offset_x = offset_x
+        self.offset_y = offset_y
 
-    def specific_loader(self, video, offset_x, offset_y):
-        X, Y = self._choose_total_frame(video, offset_x, offset_y)
-        X = convert_array(X)
-        Y = convert_array(Y)
-        return X, Y
+    def _load_frame(self, frame_path):
+        frame = Image.open(frame_path)
+        arr = np.array(frame)
+        arr = np.expand_dims(arr, -1)
+        arr = arr.astype('float32')
+        arr /= 255
+        return arr
 
-    def random_loader(self, offset_x, offset_y):
+    def _random_frames(self):
+        video_idx = np.random.randint(low=0, high=len(self.videos))
+        video = self.videos[video_idx]
+        frames = [os.path.join(video, i) for i in os.listdir(video)]
+
+        idx_y = np.random.randint(self.offset_y, len(frames), self.batch_per_video)
+        idx_x = [[y - self.offset_y + x for x in self.offset_x] for y in idx_y]
+
+        frame_y = np.array([self._load_frame(frames[i]) for i in idx_y])
+        frame_x = []
+        for x in zip(*idx_x):
+            temp_x= np.array([self._load_frame(frames[i]) for i in x])
+            frame_x.append(temp_x)
+        frame_x = np.array(frame_x)
+        return frame_x, frame_y
+
+    def random_batches(self):
         while True:
-            selected_video = self._choose_random_video()
-
-            for i in range(len(selected_video)):
-                video = selected_video[i]
-                frame_x, frame_y = self._choose_random_frame(video, offset_x, offset_y)
-
+            for i in range(int(self.batch_size/self.batch_per_video)):
+                x, y = self._random_frames()
                 if i == 0:
-                    X = frame_x
-                    Y = frame_y
+                    batch_x = x
+                    batch_y = y
                 else:
-                    X = np.concatenate((X, frame_x))
-                    Y = np.concatenate((Y, frame_y))
-
-            X = convert_array(X)
-            Y = convert_array(Y)
-            yield (X, Y)
-
-    def _choose_random_video(self):
-        video_list = os.listdir(self.directory)
-        selected_index = np.random.randint(low=0, high=len(video_list), size=int(self.batch_size / self.batch_per_video))
-        selected_video = [video_list[i] for i in selected_index]
-        return selected_video
-
-
-    def _choose_random_frame(self, video, offset_x, offset_y):
-        video_path = os.path.join(self.directory, video)
-        frame_list = os.listdir(video_path)
-
-        selected_index_y = np.random.randint(offset_y, len(frame_list), self.batch_per_video)
-        selected_index_x = [[y - offset_y + x for x in offset_x] for y in selected_index_y]
-        selected_frame_y = np.array([idx_to_array(i, video_path) for i in selected_index_y])
-        selected_frame_x = [[idx_to_array(i, video_path) for i in l] for l in selected_index_x]
-        return selected_frame_x, selected_frame_y
-
-    def _choose_total_frame(self, video, offset_x, offset_y):
-        video_path = os.path.join(self.directory, video)
-        frame_list = os.listdir(video_path)
-
-        selected_index_y = range(offset_y, len(frame_list))
-        selected_index_x = [[y - offset_y + x for x in offset_x] for y in selected_index_y]
-        selected_frame_y = np.array([idx_to_array(i, video_path) for i in selected_index_y])
-        selected_frame_x = [[idx_to_array(i, video_path) for i in l] for l in selected_index_x]
-        return selected_frame_x, selected_frame_y
-
-
-def idx_to_array(idx, video_path):
-    img_path = os.path.join(video_path, '{}.png'.format(idx))
-    im = Image.open(img_path)
-    arr = np.array(im)
-    return arr
-
-def convert_array(arr):
-    arr = np.array(arr)
-    arr = np.expand_dims(arr, -1)
-    arr = arr.astype('float32')
-    arr /= 255
-    return arr
+                    batch_x = np.concatenate((batch_x, x), axis=1)
+                    batch_y = np.concatenate((batch_y, y), axis=0)
+            yield batch_x, batch_y
