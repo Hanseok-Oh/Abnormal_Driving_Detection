902c52dcef23681a386ca93e0a9260d7aa6f5a07
YONGSUN YOON <respect5716@gmail.com>
2019-08-08 20:29:48 +0900

    update

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -46,4 +46,5 @@ class DataLoader:
                 else:
                     batch_x = np.concatenate((batch_x, x), axis=1)
                     batch_y = np.concatenate((batch_y, y), axis=0)
+            batch_x = list(batch_x)
             yield batch_x, batch_y
