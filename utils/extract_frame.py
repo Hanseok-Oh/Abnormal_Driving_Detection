773adc65f62bfed01f074b761117e29d2ea49220
YONGSUN YOON <respect5716@gmail.com>
2019-08-09 10:23:25 +0900

    update
    FROM extract_frame CREATE get_path
    tkinter gui로 path 입력받도록 함수 생성

--- utils/extract_frame.py
+++ utils/extract_frame.py
@@ -1,29 +1,28 @@
 import os
 import cv2
-import argparse
+import tkinter
 import numpy as np
 from tqdm import tqdm
 from PIL import Image
+from tkinter.filedialog import askdirectory
 
 
-# argparse
-parser = argparse.ArgumentParser()
-parser.add_argument('--load_path', help='동영상 로드 경로', type=str)
-parser.add_argument('--save_path', help='이미지 저장 경로', type=str)
-parser.add_argument('--size', help='이미지 저장 사이즈', type=int, default=256)
-args = parser.parse_args()
-
+def get_path():
+    root=tkinter.Tk()
+    root.withdraw()
+    load_path = askdirectory(title='load path')
+    save_path = askdirectory(title='save path')
+    root.destroy()
+    return load_path, save_path
 
 def get_video_list(load_path):
     video_list = os.listdir(load_path)
     return video_list
 
-
 def make_dir(save_path):
     save_path = os.path.abspath(save_path)
     if not os.path.isdir(save_path):
         os.mkdir(save_path)
-
 
 def get_video_frame(video, load_path, save_path):
     save_path = os.path.abspath(save_path)
@@ -34,27 +33,30 @@ def get_video_frame(video, load_path, save_path):
         os.mkdir(video_save_path)
 
     cap = cv2.VideoCapture(os.path.join(load_path, video))
+    fgbg = cv2.bgsegm.createBackgroundSubtractorMOG()
+
     frame_len = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
     for i in range(frame_len):
         ret, frame = cap.read()
-        frame = cv2.resize(frame, dsize=(args.size, args.size), interpolation=cv2.INTER_LINEAR)
-        frame= cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        frame = cv2.resize(frame, dsize=(256, 256), interpolation=cv2.INTER_LINEAR)
+        gray= cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        fgmask = fgbg.apply(frame)
         os.chdir(video_save_path)
-        cv2.imwrite('{}.png'.format(i), frame)
+        cv2.imwrite('{}.png'.format(i), gray)
+        cv2.imwrite('{}_fgbg.png'.format(i), fgmask)
     cap.release()
 
-
-def main(args):
-    video_list = get_video_list(args.load_path)
-    make_dir(args.save_path)
-
+def main():
+    load_path, save_path = get_path()
+    video_list = get_video_list(load_path)
+    make_dir(save_path)
     for i in tqdm(range(len(video_list)), mininterval=1):
         video = video_list[i]
-        get_video_frame(video, args.load_path, args.save_path)
+        get_video_frame(video, load_path, save_path)
 
     print('전체 완료')
 
 
 if __name__ == '__main__':
-    main(args)
+    main()
